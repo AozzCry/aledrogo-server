0069c0467fddc3655bf1cc6b1c2f13cb28180dc0
Emil <fajny.car@gmail.com>
2022-08-24 09:22:39 +0200

    procfile

--- index.js
+++ index.js
@@ -31,13 +31,6 @@ mongoose.connect(
   }
 );
 
-app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["http://localhost:3000"]);
-  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.append("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
