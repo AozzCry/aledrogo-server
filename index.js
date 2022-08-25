3ef9b51ac10557921635e914a68f24f7daa9c209
Damian Puś <35109367+DragHem@users.noreply.github.com>
2022-08-25 10:42:50 +0200

    Netlify test

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://aledrogoshop.netlify.app/",
+    origin: "https://aledrogoshop.netlify.app",
     credentials: true,
   })
 );
